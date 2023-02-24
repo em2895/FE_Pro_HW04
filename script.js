777fe63257b92a6ec7da113e3e3db938b4ce8ce2
Emil Akhmedov <emil282@ukr.net>
2023-02-24 16:52:02 +0200

    done HW_04

---
+++ script.js
@@ -0,0 +1,7 @@
+let a = Number(prompt('Введіть перше число:'));
+let b = Number(prompt('Введіть друге число:'));
+
+alert(a + " + " + b + ' = ' + [a + b]);
+alert(a + " - " + b + ' = ' + [a - b]);
+alert(a + " * " + b + ' = ' + [a * b]);
+alert(a + " / " + b + ' = ' + [a / b]);
